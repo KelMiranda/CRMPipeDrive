606169917d3adda6e8213570e70e645e9919db96
KelMiranda <kelvin.miranda@grupopelsa.com>
2023-10-24 16:09:44 -0600

    Agregando un solo Json para trabajar en Power BI

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import os
 from processes.stages import StageTable
 from processes.pipeline import PipelineTable
+from processes.deals import DealTable
+from pipedrive.pipedrive_api_conecction import PipedriveAPI
 
 
 if __name__ == '__main__':
-    PipelineTable('pipeline').validator()
-    StageTable('stages').validator()
-
-
+    #PipelineTable('pipeline').validator()
+    #StageTable('stages').validator()
+    DealTable('deals').make_all_deals()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
