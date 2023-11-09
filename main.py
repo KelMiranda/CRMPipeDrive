d7933aa9da837247a44d429c33970e61a48be7fd
KelMiranda <kelvin.miranda@grupopelsa.com>
2023-11-08 19:42:03 -0600

    Subiendo

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from processes.deals import get_all_deals
 from pipedrive.pipedrive_api_conecction import PipedriveAPI
 from processes.deals import DealTable
 from processes.organizations import get_all_organization
+from processes.organizations import OrganizationTable
 
 
 if __name__ == '__main__':
@@ -14,13 +15,7 @@ if __name__ == '__main__':
     #print(dictionary_invert(get_all_option_for_fields_in_deals([12527, 12546, 12521, 12523]).get('12523'), 180))
     #print(PipedriveAPI('TOKEN_CRM').get_deals(2287).get('data').get('6fe64586c7f0e32e9caabde4b5c1d7a2ea697748'))
     #DealTable('DatosProyectos_PipeDrive', 'GT').distinct()
-    print(get_all_organization())
-    print(len(get_all_organization()))
-
-    result = DealTable('DatosProyectos_PipeDrive', 'GT').order_by_doc_status()
-    print(result[0].get('583'))
-    for row in result[0]:
-        print(row)
+    OrganizationTable().client_validator()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- processes/organizations.py
+++ processes/organizations.py
@@ -1,5 +1,8 @@
+import time
 from pipedrive.pipedrive_api_conecction import PipedriveAPI
 from database.sql_server_connection import SQLServerDatabase
+from processes.deals import get_all_option_for_fields_in_deals
+
 
 
 def get_all_organization():
@@ -13,7 +16,16 @@ def get_all_organization():
 
 
 class OrganizationTable:
-    def __int__(self, table, country):
+    def __int__(self, table):
         self.table = table
-        self.country = country
         self.database = SQLServerDatabase('SERVER', 'DATABASE','USERNAME_', 'PASSWORD')
+
+    def client_validator(self):
+        pais = 'sv'
+        cardcode = 'C000000'
+        result_pipedrive = get_all_organization()
+        for row in result_pipedrive:
+            time.sleep(1)
+            print(row.get('bd4aa325c2375edc367c1d510faf509422f71a5b'))
+        #query = f"Select * from {self.table} Where Pais = '{pais}' AND CardCode = '{cardcode}'"
+        #self.database.execute_query(query)
