091c94696ac0ea0234e2b9af0371d5f05e74e844
Kelvin Miranda <kelmiranda88@icloud.com.com>
2023-12-13 23:56:39 -0600

    Jinja2 tiene problemas

--- mail/JinjaTemplateManager.py
+++ mail/JinjaTemplateManager.py
@@ -1,17 +1,20 @@
+
 from jinja2 import Environment, FileSystemLoader
 
-class TemplateRenderer:
-    def __init__(self, template_dir='./templates', default_template='base.html'):
-        # Inicializa el entorno de Jinja2 con el directorio de plantillas
-        self.env = Environment(loader=FileSystemLoader(template_dir))
-        self.default_template = default_template
 
-    def load_template(self, template_name=None):
-        # Carga una plantilla específica, o la predeterminada si no se proporciona una
-        template_name = template_name or self.default_template
-        return self.env.get_template(template_name)
+if __name__=='__main__':
+    # Carga el entorno y la plantilla
+    env = Environment(loader=FileSystemLoader('./templates'))
+    template = env.get_template('base_correo.html')
 
-    def render(self, template_name=None, **kwargs):
-        # Carga y renderiza la plantilla con los datos proporcionados
-        template = self.load_template(template_name)
-        return template.render(**kwargs)
+    # Producto en un diccionario.
+    productos = {
+    1: {'nombre': 'Manzana', 'precio': 0.50, 'descripción': 'Roja y jugosa'},
+    2: {'nombre': 'Banana', 'precio': 0.30, 'descripción': 'Madura y dulce'},
+    3: {'nombre': 'Cereza', 'precio': 2.00, 'descripción': 'Fresca y deliciosa'}
+}
+
+    # Renderiza la plantilla con datos
+    html = template.render(nombre_vendedor="Nombre Vendedor", productos=productos)
+
+    print(html)
