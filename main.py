310dd6ee4fc7f328527f238c229541a29f50f215
Kelvin Miranda <kelmiranda88@icloud.com.com>
2023-12-19 18:55:19 -0600

    Solucionado

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from processes.cotizaciones import Cotizaciones
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     db = Cotizaciones('SV')
     db.validando_cotizacion()
-=======
-    pass
->>>>>>> 7cbb733f1069a0b28bc497e5cee31a3a0e994ac8
