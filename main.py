36daec8a321ff1aa3e1bebc778fa3cfc05c06f3c
KelMiranda <kelvin.miranda@grupopelsa.com>
2023-12-20 16:50:10 -0600

    Agregando la funcion final de cierre de cotizaciones

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from processes.cotizaciones import Cotizaciones
+from mail.JinjaTemplateManager import JinjaTemplateManager
+from datetime import datetime
+from mail.mail import Mail
 
 if __name__ == '__main__':
-    db = Cotizaciones('SV')
-    print(db.validar_cotizacion())
+    datos = {}
+    pais = ['SV', 'GT']
+    cont = 0
+    fecha_actual = datetime.now().strftime("%d/%m/%Y")
+    for row in pais:
+        proceso = Cotizaciones(f'{row}').validar_cotizacion()
+        cont = cont + 1
+        datos[cont] = {'#documentos': proceso[0],
+                    '#errores': proceso[1]
+                    }
+    
+    contenido = JinjaTemplateManager()
+    mail = Mail('SMTP_SERVER', 'SMTP_USERNAME', 'SMTP_PASSWORD')
+    html = contenido.render_template(template_name='notificacion.html', nombre_vendedor = 'Kelvin Miranda', fecha_actual=fecha_actual, datos=datos)
+    mail.enviar_correo('kelvin.miranda@grupopelsa.com', 'Automatizacíon Cierre de cotización', html)
+        
+
+    
+        
--- processes/cotizaciones.py
+++ processes/cotizaciones.py
@@ -1,4 +1,5 @@
 from database.sql_server_connection import SQLServerDatabase
+from time import sleep
 
 
 def notificar_errores(errores):
@@ -14,57 +15,73 @@ class Cotizaciones:
     
 
     def cierre_de_cotizaciones(self):
-        self.db.connect()
-        query = f'EXEC [dbo].[SP_CERRANDO_COTIZACIONES_{self.pais}]'
-        result = self.db.execute_query(query)
-        self.db.disconnect()
-        return result
-    
-    def validar_cotizacion(self, ord=None, docnum=None, serie=None):
-        documentos_actualizados = []
         errores = []
+        query = f'EXEC [dbo].[SP_CERRANDO_COTIZACIONES_{self.pais}]'
 
-        if ord is None and docnum is None and serie is None:
-            result = self.cierre_de_cotizaciones()
-            total = len(result)
+        try:
+            self.db.connect()
+            result = self.db.execute_query(query)
 
-            for count, row in enumerate(result, start=1):
-                iter = total - count
-                print(f"-------------#Documentos Faltantes: {iter}---------------------------")
-                query = f"EXEC [dbo].[SP_VALIDADOR_PROYECTO_MERGE_{self.pais}]'{row[2]}',{row[3]},'{row[4]}'"
-                try:
-                    self.db.connect()
-                    query_result = self.db.execute_query(query)
-                    documentos_actualizados.append(row[3])
-                    if not query_result:
-                        errores.append(f"Consulta vacía en iteración {count}")
-                except Exception as e:
-                    error_message = f"Error al ejecutar consulta en iteración {count}: {str(e)}"
-                    print(error_message)
-                    errores.append(error_message)
-                finally:
-                    self.db.disconnect()
-
-        else:
-            query = f"EXEC [dbo].[SP_VALIDADOR_PROYECTO_MERGE_{self.pais}]'{ord}',{docnum},'{serie}'"
-            print(query)
-            try:
-                self.db.connect()
-                query_result = self.db.execute_query(query)
-                if not query_result:
-                    errores.append("Consulta vacía")
-                documentos_actualizados.append(docnum)
-            except Exception as e:
-                error_message = f"Error al ejecutar consulta: {str(e)}"
-                print(error_message)
-                errores.append(error_message)
-            finally:
-                self.db.disconnect()
-
-        print('######################Finalizando Proceso##########################################')
         
-        # Enviar notificación si hay errores
+        except Exception as e:
+            error_message = f"Error al ejecutar la consulta dicho error es: {str(e)}"
+            errores.append(error_message)
+
+        finally:
+            self.db.disconnect()
+        
         if errores:
             notificar_errores(errores)
         
-        return documentos_actualizados
+        return result, errores
+    
+
+    def validar_cotizacion(self, ord=None, docnum=None, serie=None):
+        documentos_actualizados = []
+        errores = []
+        if ord is None and docnum is None and serie is None:
+            result = self.cierre_de_cotizaciones()
+            if len(result[0]) == 0:
+                documentos_actualizados.append(f'No hay cotizaciones en la funcion de cierre en: {self.pais}')
+                errores.append(f"No hay error en la funcion de cierre en: {self.pais}")
+            else:
+                total = len(result[0])
+                for count, row in enumerate(result[0], start=1):
+                    iter = total - count
+                    print(f"-------------#Documentos Faltantes: {iter}---------------------------")
+                    query = f"EXEC [dbo].[SP_VALIDADOR_PROYECTO_MERGE_{self.pais}]'{row[2]}',{row[3]},'{row[4]}'"
+                    try:
+                        self.db.connect()
+                        query_result = self.db.execute_query(query, False)
+                        documentos_actualizados.append(row[3])
+                        if query_result is not None:
+                            errores.append(f"Consulta vacía en iteración {count}")
+                    except Exception as e:
+                        error_message = f"Error al ejecutar consulta en iteración {count}: {str(e)}"
+                        errores.append(error_message)
+                    finally:
+                        self.db.disconnect()
+
+        else:
+            query = f"EXEC [dbo].[SP_VALIDADOR_PROYECTO_MERGE_{self.pais}]'{ord}',{docnum},'{serie}'"
+            query_validador = f"Select * from DatosProyectos_PipeDrive Where DocNum = {docnum} AND ORD ='{ord}'"
+            self.db.connect()
+            query_validador_result = self.db.execute_query(query_validador)
+            if query_validador_result:
+                try:
+                    query_result = self.db.execute_query(query, False)
+                    if query_result is not None:
+                        errores.append("Consulta vacía")
+                    documentos_actualizados.append(docnum)
+                except Exception as e:
+                    error_message = f"Error al ejecutar consulta: {str(e)}"
+                    errores.append(error_message)
+                finally:
+                    self.db.disconnect()
+            else:
+                error_message = f"El numero de documento: {docnum}, no pertenece a la tabla ORD{ord}"
+                errores.append(error_message)
+
+        print('######################Finalizando Proceso##########################################')
+        return documentos_actualizados, errores
+   
