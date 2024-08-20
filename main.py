ab8363b289a2dd08bc04d63092799569f1f6d975
KMiranda <kelvin.miranda@grupopelsa.com>
2024-08-20 16:42:06 -0600

    Agregando en la pc de casa

--- main.py
+++ main.py
@@ -11,17 +11,10 @@ from processes.proceso_cliente import Cliente
 
 if __name__ == '__main__':
 
-    #pais = ['SV', 'GT']
-    '''for row in pais:
+    '''pais = ['SV', 'GT']
+    for row in pais:
         ct = IngresoDeCotizaciones(f'{row}')
         result = ct.cotizaciones_diarias(1)
         result_act = ct.cotizaciones_actualizadas()
         save_json(result, f'Cotizaciones_diarias_{row}')
         save_json(result_act, f'Cotizaciones_actualizadas_{row}')'''
-    IngresoDeCotizaciones('SV').cotizacionesDiarias()
-
-
-
-
-
-
