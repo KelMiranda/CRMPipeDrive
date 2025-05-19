75dd0e1e8d7b5ab320e81dc0678582072457248d
KMiranda <kelvin.miranda@grupopelsa.com>
2025-05-19 11:40:19 -0600

    Remover carpeta del seguimiento y agregar a .gitignore

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ if __name__ == '__main__':
     #print(datos)
 
 
-    pais = ['SV', 'GT', 'HN']
+    '''pais = ['SV', 'GT', 'HN']
     for row in pais:
         try:
             print(f"#############################Inicio de los proceso para {row}###################################")
             ct = IngresoDeCotizaciones(f'{row}')
-            print(ct.proceso_clientes_dias(1))
+            print(ct.proceso_clientes_dias(2))
             print(f"#####################Finalizando proceso clientes dias para {row}###############################")
             print(ct.cotizaciones_actualizadas())
             print(f"#####################Finalizando cotizaciones actualizadas para {row}###########################")
-            print(ct.proceso_cotizaciones_dia(1))
+            print(ct.proceso_cotizaciones_dia(2))
             print(f"#####################Finalizando proceso cotizaciones dias para {row}###########################")
             print(ct.proceso_cotizaciones_pipedrive())
             print(f"#####################Finalizando proceso cotizaciones pipedrive para {row}######################")
@@ -40,9 +40,10 @@ if __name__ == '__main__':
             print(f'Ocurrió un error con el país {row}: {e}')
 
     bot = TelegramBot(None)
-    bot.send_message(1947314689)
+    bot.send_message(1947314689)'''
 
-    #print(get_all_deals())
+    all_deals = get_all_deals()
+    #pd.DataFrame(all_deals).to_json()
 
     #print(Cliente('SV').ingresando_cliente('C5223'))
     #print(Cliente('SV').validadorCliente('C5223'))
